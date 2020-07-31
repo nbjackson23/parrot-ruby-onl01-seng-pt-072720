36110317568dff217567fba4bce89ed609753880
Nancy Jackson <nbjackson23@gmail.com>
2020-07-31 03:30:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot (phrase)
-  phrase = "Pretty bird!"
+def parrot 
+  phrase = ("Pretty bird!")
   puts "Squawk!"
   "Squawk!"
 end
